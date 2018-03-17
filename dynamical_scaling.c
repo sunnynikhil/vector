654468571b6ccd63c8f05e8c20e6298364a02bf7
sunnynikhil <37411671+sunnynikhil@users.noreply.github.com>
2018-03-17 23:16:13 +0530

    Update dynamical_scaling.c

--- dynamical_scaling.c
+++ dynamical_scaling.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 typedef struct vector
 {
-    int *arr;
+    void **arr;
     int curLen;
     int totalSize;
 } vector;
@@ -21,31 +21,36 @@ void resize(vector *vec)
     vec->arr=(int*)realloc(vec->arr,sizeof(int)*(vec->curLen+1));
     return;
 }
-void insert(vector *vec,int key)
+void insert(vector *vec,void *key,size_t size)
 {
 
     resize(vec);
-    vec->arr[vec->curLen]=key;
+    vec->arr[vec->curLen]=malloc(size);
+    memcpy(vec->arr[vec->curLen],key,size);
     vec->curLen=vec->curLen+1;
     return;
 }
 
-void display(vector *vec)
+void display(vector *vec,void (*vp)(void*))
 {
     int index;
     for(index=0; index<vec->curLen; index++)
     {
-        printf("%d\t",vec->arr[index]);
+       (*vp)(vec->arr[index]);
     }
 }
+void display_n(void *n)
+{
+    printf("      %d  ",*(int *)n);
+}
 
-
-void delete(vector *vec,int key)
+void delete(vector *vec,void *key,size_t size)
 {
     int index,found=0,pos;
     for(index=0; index<vec->curLen; index++)
     {
-        if(vec->arr[index]==key)
+
+        if(convert(vec->arr[index],size)==convert(key,size))
         {
 
             found=1;
@@ -66,6 +71,21 @@ void delete(vector *vec,int key)
         printf("\n*key is not found*\n");
     }
 }
+int convert(void *data,size_t size)
+{
+    if (size == 4)
+    {
+        int idx = *(int*)data;
+        return idx;
+    }
+    else
+    {
+        printf("here\n");
+        int value = *(int*)data;
+        printf("%d\n",value);
+        return value;
+    }
+}
 int main()
 {
     vector * vec = createVector(10);
@@ -81,21 +101,21 @@ int main()
        switch(choice)
        {
        case 1:
-        printf("\n enter the value to insert\n");
+        printf("\nenter the value to insert\n");
         scanf("%d",&value);
-        insert(vec,value);
+        insert(vec,&value,sizeof(int));
         break;
        case 2:
         printf("\nenter the to delete\n");
         scanf("%d",&value1);
-        delete(vec,value1);
+        delete(vec,&value1,sizeof(int));
         break;
        case 3:
-        display(vec);
+        display(vec,display_n);
         break;
        case 4:
         exit(0);
        }
    }
-
+return 0;
 }
